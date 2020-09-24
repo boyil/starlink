1c387704be20ba2512fec42d8192335b3552a967
boyil <boyi.l.dimi@gmail.com>
2020-09-23 21:16:40 -0700

    Build overall layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Layout } from 'antd';
+import starlinkLogo from './images/starlink_logo.svg';
+import Main from "./components/Main";
+
+const { Header, Footer, Content } = Layout;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Layout>
+        <Header>
+            <img src={starlinkLogo} className="App-logo" alt="logo" />
+            <p className="title">StarLink Tracker</p>
+        </Header>
+        <Content>
+            <Main />
+        </Content>
+        <Footer>
+            (c)2020 StarLink Tracker. All Rights Reserved. Website Made by XXX.
+        </Footer>
+      </Layout>
   );
 }
 
