1a854ab15c0d6a245b288ba73d04e907dffa4805
boyil <boyi.l.dimi@gmail.com>
2020-09-26 15:56:56 -0700

    Add satellite list component

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import {Col, Row} from "antd";
 import ObserverInfo from "./ObserverInfo";
+import { SAT_CATEGORY, N2YO_API_KEY, N2YO_BASE_URL } from "../constants";
+import SatelliteList from "./SatelliteList";
+
+export const ABOVE_API_BASE_URL = `${N2YO_BASE_URL}/above`;
 
 const Main = () => {
+    const [satList, setSatList] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    const findSatellitesOnClick = (nextObserverInfo) => {
+        const { longitude, latitude, altitude, radius } = nextObserverInfo;
+
+        setLoading(true);
+        fetch(`${ABOVE_API_BASE_URL}/${latitude}/${longitude}/${altitude}/${radius}/${SAT_CATEGORY}&apiKey=${N2YO_API_KEY}`)
+            .then(response => response.json())
+            .then(data => {
+                setSatList(data.above.map((satellite) => ({
+                    ...satellite,
+                    selected: false
+                })));
+                setLoading(false);
+            })
+            .catch(() => {
+                setLoading(false);
+                alert("N2YO API call fails. Please try again.");
+            });
+    }
+
     return (
         <Row>
             <Col span={8}>
-                <ObserverInfo />
+                <ObserverInfo findSatellitesOnClick={findSatellitesOnClick} loading={loading} />
+                <SatelliteList satList={satList} updateSatelliteList={setSatList} loading={loading} />
             </Col>
             <Col span={16}>
                 WorldMap
